cfd08c45a036f8604237c43c5c281a19dd85ba9a
Varun Gumma <45076943+Kudo-kun@users.noreply.github.com>
2020-09-18 21:21:18 +0530

    Update rdts.py

--- rdts.py
+++ rdts.py
@@ -25,14 +25,7 @@ class RDTSocket:
         self._read_buffer = dict()
         self._write_buffer = dict()
 
-    def _get_ip():
-
-        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        sock.connect(("10.255.255.255", 1))
-        ip = sock.getsockname()[0]
-        sock.close()
-        return ip
-
+    
     def initialize(self):
 
         self._socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
